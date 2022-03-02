4f6f29985ba5bc24eee4f2562bcb2f563ab5adc3
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2022-03-02 13:31:40 -0800

    change absolute positioning for flexbox

--- src/DateTime/DateTime.tsx
+++ src/DateTime/DateTime.tsx
@@ -3,10 +3,10 @@ import styles from "./DateTime.module.css";
 type DateTimeProps = { date: string; time: string };
 const DateTime = ({ date, time }: DateTimeProps) => {
   return (
-    <>
+    <div className={ [styles.container].join(" ") }>
       <div className={ [styles.date, "text"].join(" ") }>{ date }</div>
       <p className={ [styles.time, "text"].join(" ") }>{ time }</p>
-    </>
+    </div>
   );
 };
 
