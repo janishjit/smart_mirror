0c95492a866b6f825009def44843f56a01b37a9b
TrevorFlanigan <trevorflanigan@alumni.ubc.ca>
2022-03-01 21:21:00 -0800

    adds react boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+// Look at figma for modules, claim modules with a comment on the figma file
+// https://www.figma.com/file/yrqrIB1452Kw8NM9943uOt/Magic-Mirror-Wireframes?node-id=2%3A17
+function App() {
+  return (
+    <div className="App">
+      <div className="col1">left column</div>
+      <div className="reflection-area">reflection area</div>
+      <div className="col3">right column</div>
+    </div>
+  );
+}
+
+export default App;
