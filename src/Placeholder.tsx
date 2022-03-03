cc82bf481d3a600205addea13bba52a22f2d1039
janishjit <65305278+janishjit@users.noreply.github.com>
2022-03-02 22:21:11 -0800

    Pushed server

--- src/Placeholder.tsx
+++
@@ -1,7 +0,0 @@
-type PlaceholderProps = {};
-
-const Placeholder = (props: PlaceholderProps) => {
-  return <div style={ { flex: 1 } }>Placeholder</div>;
-};
-
-export default Placeholder;
--- src/types.d.ts
+++
@@ -1,4 +0,0 @@
-export type EventType = {
-  time: string;
-  event: string;
-};
